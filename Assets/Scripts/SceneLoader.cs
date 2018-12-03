4c1c58c1f00a74e1321449c9194e19c6d0be9ffb
Henrique H. da Silva Fortes <fortes_henrique@hotmail.com>
2018-12-03 15:31:19 -0200

    Add Project

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour {
+
+    public void  LoadNextScene()
+    {
+        //Carrega as próximas cenas
+        int currenteSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currenteSceneIndex + 1);
+    }
+
+    public void LoadStartScene()
+    {       
+        //Carrega a cena de começo do jogo "StartScene"
+        SceneManager.LoadScene(0);
+    }
+
+}
